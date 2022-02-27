9b04df130520a61392c9078a63bebb2e6cf72c91
Patrick <janpatrick.dev@gmail.com>
2022-02-27 13:56:54 +0800

    add PATCH and DELETE method on cards, create edit popup, minor fixes

--- frontend/src/AddNote.js
+++ frontend/src/AddNote.js
@@ -37,11 +37,11 @@ function AddNote(props) {
     { visible ? <div className="add-note__overlay" onClick={handleOverlayClick}></div> : null }
     <div className="add-note__container">
       <div className="add-note__input-container">
-        { visible ?  <input type='text' name='title' placeholder='Title' value={title} onChange={handleTitleChange} className="add-note__title-input" /> : null }
-        <textarea placeholder='Take a note...' value={content} onChange={handleContentChange} onClick={handleTextAreaClick} className="add-note__content-textarea" />
+        { visible ?  <input maxLength="1000" type='text' name='title' placeholder='Title' value={title} onChange={handleTitleChange} className="add-note__title-input" /> : null }
+        <textarea maxLength="20000" placeholder='Take a note...' value={content} onChange={handleContentChange} onClick={handleTextAreaClick} className="add-note__content-textarea" />
       </div>
       <div>
-        <button className="add-note__btn" onClick={handleAddClick} aria-label="Add Note">
+        <button className="add-note__btn" onClick={title || content ? handleAddClick : null} aria-label="Add Note">
           <AddIcon />
         </button>
       </div>
--- frontend/src/Note.js
+++ frontend/src/Note.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Note(props) {
-  return (
-    <div className="card__container">
+
+  function handleEditClick() {
+    props.onEditClick(props.note);
+  }
+
+  return props.title || props.content ? (
+    <div className="card" onClick={handleEditClick}>
       { props.title ? <h1 className="card__title">{props.title}</h1> : null }
+      <button className="card__edit-btn" aria-label="Edit Note" onClick={handleEditClick}>
+        <EditIcon />
+      </button>
       <span>
         <pre className="card__content">
           {props.content}
         </pre>
       </span>
     </div>
-  );
+  ) : (
+    <div className="card" onClick={handleEditClick}>
+      <h1 className="card__empty">Empty note</h1>
+      <button className="card__edit-btn" aria-label="Edit Note" onClick={handleEditClick}>
+        <EditIcon />
+      </button>
+    </div>
+  )
 }
 
 export default Note;
